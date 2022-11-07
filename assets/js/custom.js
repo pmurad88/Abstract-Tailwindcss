36e9301bdf6ccba53a104117db511f4bebbf0f9b
pmurad88 <pmurad88@gmail.com>
2022-11-08 01:01:13 +0600

    third

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,16 @@ $(document).ready(function(){
         $(".mobile-menu").stop(true,false,true).slideToggle();
     });
     
-
+    $('body').prepend('<div class="go-top"><i class="fa-solid fa-angle-up"></i></div>')
+     $(window).scroll(function(){
+        if($(window).scrollTop() > 200){
+            $(".go-top").fadeIn();
+        }else{
+            $(".go-top").fadeOut();
+        }
+     });
+     $(".go-top").on('click', function(){
+        $("body, html").animate({scrollTop:0}, 999);
+     });
 
 });
